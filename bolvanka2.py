012fc91ba3d175308c10c7a97c1ccaa4897896aa
Zakuskin Alexander <110430184+procsimo7@users.noreply.github.com>
2022-09-13 01:18:09 +0300

    Итоговое задание 12.7.3

---
+++ bolvanka2.py
@@ -0,0 +1,16 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+money = int(input("Введите сумму:"))
+
+tkb = money * (per_cent.get('ТКБ')/100)
+skb = money * (per_cent.get('СКБ')/100)
+vtb = money * (per_cent.get('ВТБ')/100)
+sber = money * (per_cent.get('СБЕР')/100)
+
+deposit = [tkb, skb , vtb, sber]
+
+print(deposit)
+
+print(max(deposit))
+
+
